8768bc40fdb47edb1ff4faf1cd0b62e88fec7c49
Alexia Hernandez <alexia.hernandez.17@tfacorps.org>
2018-06-25 13:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your code in this file!
+const currentUser = 'Alexia Hernandez';
+
+const shortGreeting = `Welcome, ${currentUser}`;
+
+'Alexia Hernandez'.length;
+
+'Alexia Hernandez'[15]; 
+
+'Alexia Hernandez'[14];
+
+'Alexia Hernandez'.slice(0,3);
+
+currentUser.slice(0,1)
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+
