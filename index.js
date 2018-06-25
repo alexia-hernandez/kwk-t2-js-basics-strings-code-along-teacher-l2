0069657e4c780c99659dcb19c513e549ab432a1f
Alexia Hernandez <alexia.hernandez.17@tfacorps.org>
2018-06-25 13:49:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const currentUser = 'Alexia Hernandez';
 
 const shortGreeting = `Welcome, ${currentUser.slice(0.1)}!`;
 
-'Alexia Hernandez'.length;
-
-'Alexia Hernandez'[15]; 
-
-'Alexia Hernandez'[14];
-
-'Alexia Hernandez'.slice(0,3);
-
 currentUser.slice(0, 1);
 
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
