9d6ff635d8f02356ac25e03607e7918982b002d0
Alexia Hernandez <alexia.hernandez.17@tfacorps.org>
2018-06-25 13:52:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+const currentUser = 'Alexia Hernandez';
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+currentUser.slice(0, 1);
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
